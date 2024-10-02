a0d786e39ae7447f9681d782fef76b0a30d1f0eb
Franco Iván Ferreyra <fiferreyra@fi.uba.ar>
2024-10-02 19:27:39 -0300

    Englobar al estado en un TDA propio para reutilizar codigo en los TDA Ficha, Celda y Jugador

--- src/Estado.java
+++ src/Estado.java
@@ -1,4 +1,34 @@
-public enum Estado {
-    NORMAL,
-    BLOQUEADO
+
+
+public class Estado{
+    private TipoEstado tipo;
+
+    public Estado(TipoEstado tipo) throws Exception
+    {
+        if(tipo == null)
+        {
+            throw new Exception("El tipo de estado no debe ser nulo");
+        }
+        this.tipo = tipo;
+    }
+
+    public Estado() throws Exception
+    {
+        this(TipoEstado.NORMAL);
+    }
+
+    public TipoEstado obtenerTipoEstado()
+    {
+        return this.tipo;
+    }
+
+    public void bloquear()
+    {
+        this.tipo = TipoEstado.BLOQUEADO;
+    }
+
+    public void desbloquear()
+    {
+        this.tipo = TipoEstado.NORMAL;
+    }
 }
