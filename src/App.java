f4126c47e3e56467aec16c5ec52111dbb3809a12
Franco Iván Ferreyra <fiferreyra@fi.uba.ar>
2024-11-11 22:05:03 -0300

    Llamar a la funcion de jugar de Tateti en el main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import tateti.Tateti;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Tateti tateti = new Tateti();
+
+        tateti.jugar();
     }
 }
