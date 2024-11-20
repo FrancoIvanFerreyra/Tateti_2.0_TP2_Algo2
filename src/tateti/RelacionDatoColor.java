a7c0c1731460cc0b59cf6d590b81f67ca3a7a595
Franco Iván Ferreyra <fiferreyra@fi.uba.ar>
2024-11-20 15:22:59 -0300

    Agregar pre, pos y validaciones al TDA RelacionDatoColor

--- src/tateti/RelacionDatoColor.java
+++ src/tateti/RelacionDatoColor.java
@@ -1,6 +1,7 @@
 package tateti;
 
 import java.awt.Color;
+import utiles.ValidacionesUtiles;
 
 public class RelacionDatoColor<T> {
 	private T dato = null;
@@ -8,8 +9,11 @@ public class RelacionDatoColor<T> {
 	
 //CONSTRUCTORES -------------------------------------------------------------------------------------------
 	
-	public RelacionDatoColor() {}
-	
+	/*
+	pre: color no puede ser null, dato no puede ser null
+	pos: crea una instancia intermedia que vincula a un dato con el color
+		 que representa
+	*/	
 	public RelacionDatoColor(Color color, T dato) {
 		this.color = color;
 		this.dato = dato;
@@ -20,19 +24,37 @@ public class RelacionDatoColor<T> {
 //METODOS DE COMPORTAMIENTO -------------------------------------------------------------------------------
 //GETTERS SIMPLES -----------------------------------------------------------------------------------------
 
+	/*
+	 * pre: -
+	 * pos: devuelve el dato
+	 */
 	public T getDato() {
 		return this.dato;
 	}
+	/*
+	 * pre: -
+	 * pos: devuelve el color
+	 */
 	public Color getColor() {
 		return this.color;
 	}
 	
 //SETTERS SIMPLES -----------------------------------------------------------------------------------------	
 
-    public void setDato(T dato) {
+	/*
+	 * pre: dato no púede ser null
+	 * pos: actualiza el dato que representa el color
+	 */
+    public void setDato(T dato) throws Exception {
+		ValidacionesUtiles.validarNoNull(dato, "dato");
         this.dato = dato;
     }
-    public void setColor(Color color) {
+	/*
+	 * pre: color no púede ser null
+	 * pos: actualiza el color representado por el dato
+	 */
+    public void setColor(Color color) throws Exception{
+		ValidacionesUtiles.validarNoNull(color, "color");
         this.color = color;
     }
 }
