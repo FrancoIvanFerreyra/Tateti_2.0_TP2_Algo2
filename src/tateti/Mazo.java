86c0a1c9b3533586626dc76f1511a46eccfd1169
Franco Iván Ferreyra <fiferreyra@fi.uba.ar>
2024-11-22 15:45:33 -0300

    Agregar pre, pos y validaciones de excepciones descriptivas al TDA Mazo

--- src/tateti/Mazo.java
+++ src/tateti/Mazo.java
@@ -2,101 +2,141 @@ package tateti;
 
 
 
+import cartas.*;
 import estructuras.Cola;
 import estructuras.Lista;
 import estructuras.ListaSimplementeEnlazada;
-
-
-import cartas.*;
 import interfaz.Consola;
-import java.util.Random;
+import java.lang.reflect.InvocationTargetException;
+import java.util.NoSuchElementException;
+import utiles.Utiles;
+import utiles.ValidacionesUtiles;
 
 public class Mazo {
     private int tamanio;
     private Cola<Carta> cartasDisponibles;
     private Lista<Carta> cartasUtilizadas;
 
-    public Mazo(int tamanio)
+    /**
+     * Crea un mazo de cartas de tamanio maximo tamanio, donde cada carta es instanciada
+     * al azar de entre todos los tipos de cartas existentes y guardada en el mazo. Tambien
+     * crea una lista de cartas utilizadas que se utilizara para descartar las cartas que se
+     * hayan usado
+     * @param tamanio mayor o igual a 1
+     * @throws IllegalArgumentException si tamanio es menor a 1
+     */
+    public Mazo(int tamanio) throws IllegalArgumentException
     {
         this.tamanio = tamanio;
-        this.cartasDisponibles = new Cola<Carta>();
-        this.cartasUtilizadas = new ListaSimplementeEnlazada<Carta>();
+        this.cartasDisponibles = new Cola<>();
+        this.cartasUtilizadas = new ListaSimplementeEnlazada<>();
         Lista<Class<? extends Carta>> tiposDeCarta = obtenerTiposDeCarta();
-        Random random = new Random();
-        for(int i = 0; i < tamanio; i++)
+
+        for(int i = 0; i < this.tamanio; i++)
         {
-            int indiceRandom = random.nextInt(tiposDeCarta.getTamanio()) + 1;
+            int indiceRandom = Utiles.obtenerEnteroAleatorio(1, tiposDeCarta.getTamanio());
             try {
-                Carta nuevaCarta = tiposDeCarta.obtener(indiceRandom).getDeclaredConstructor().newInstance(null);
+                Carta nuevaCarta = tiposDeCarta.obtener(indiceRandom).
+                                    getDeclaredConstructor().
+                                    newInstance();
                 cartasDisponibles.acolar(nuevaCarta);
-            } catch (Exception e) {
+            } catch (IllegalAccessException | IllegalArgumentException | 
+                    InstantiationException | NoSuchMethodException | 
+                    NullPointerException | SecurityException | 
+                    InvocationTargetException e) {
+                Consola.imprimirMensaje("No se pudo crear la carta para el mazo" + e.getMessage());
             }
         }
     }
+
+    /**
+     * 
+     * @return devuelve una lista con todos los tipos de cartas existentes
+     */
     private Lista<Class<? extends Carta>> obtenerTiposDeCarta()
     {
-        Lista<Class<? extends Carta>> tiposDeCarta = new ListaSimplementeEnlazada<Class<? extends Carta>>();
-        try
-        {
-            tiposDeCarta.agregar(CartaPierdeTurno.class);
-            tiposDeCarta.agregar(CartaDuplicarTurno.class);
-            tiposDeCarta.agregar(CartaBloquearFicha.class);
-            tiposDeCarta.agregar(CartaAnularCasillero.class);
-            tiposDeCarta.agregar(CartaRetrocederUnaJugada.class);
-            tiposDeCarta.agregar(CartaTeletransportarFicha.class);
-            tiposDeCarta.agregar(CartaCambiarColorFicha.class);
-        }
-        catch(Exception e)
-        {
-
-        }
-
+        Lista<Class<? extends Carta>> tiposDeCarta = new ListaSimplementeEnlazada<>();
+        
+        tiposDeCarta.agregar(CartaPierdeTurno.class);
+        tiposDeCarta.agregar(CartaDuplicarTurno.class);
+        tiposDeCarta.agregar(CartaBloquearFicha.class);
+        tiposDeCarta.agregar(CartaAnularCasillero.class);
+        tiposDeCarta.agregar(CartaRetrocederUnaJugada.class);
+        tiposDeCarta.agregar(CartaTeletransportarFicha.class);
+        tiposDeCarta.agregar(CartaCambiarColorFicha.class);
+        
         return tiposDeCarta;
     }
 
-    public Carta levantarCarta() throws Exception
+    /**
+     * 
+     * @return levanta una carta del frente del mazo de cartas disponibles
+     * @throws NoSuchElementException si no hay cartas disponibles
+     */
+    public Carta levantarCarta() throws NoSuchElementException
     {
         if(!hayCartasDisponibles())
         {
-            throw new Exception("No hay cartas disponibles en el mazo");
+            throw new NoSuchElementException("No hay cartas disponibles en el mazo");
         }
         return this.cartasDisponibles.desacolar();
     }
 
-    public void devolverCarta(Carta carta) throws Exception
+    /**
+     * Retorna una carta al fondo del mazo de cartas disponibles
+     * @param carta no debe ser null
+     * @throws NullPointerException si carta es null
+     * @throws IllegalStateException si el mazo ya tiene el maximo de cartas disponibles
+     */
+    public void devolverCarta(Carta carta) throws NullPointerException, IllegalStateException
     {
-        if(carta == null)
+        ValidacionesUtiles.validarNoNull(carta, "carta");
+        if(estaCompleto())
         {
-            throw new Exception("La carta no puede ser null");
+            throw new IllegalStateException("El mazo ya tiene el maximo de cartas");
         }
         this.cartasDisponibles.acolar(carta);
     }
 
-    public void descartarCarta(Carta carta) throws Exception
+    /**
+     * Arroja la carta a la lista de cartas utilizadas
+     * @param carta no puede ser null
+     * @throws NullPointerException si carta es null
+     * @throws IllegalStateException si ya se descarto todas las cartas del mazo
+     */
+    public void descartarCarta(Carta carta) throws NullPointerException, IllegalStateException
     {
-        if(carta == null)
+        ValidacionesUtiles.validarNoNull(carta, "carta");
+        if(this.cartasUtilizadas.getTamanio() == this.tamanio)
         {
-            throw new Exception("La carta no puede ser null");
+            throw new IllegalStateException("Ya se descartaron todas las cartas del mazo");
         }
         this.cartasUtilizadas.agregar(carta);
     }
 
-    public void mezclar() throws Exception
+    /**
+     * Toma las cartas utilizadas, las intercambia de indices al azar y las
+     * guarda en el mazo de cartas disponibles. No debe haber cartas disponibles
+     * para que el mazo se pueda mezclar
+     * @throws IllegalStateException si el mazo aun tiene cartas disponibles
+     */
+    public void mezclar() throws IllegalStateException
     {
         if(hayCartasDisponibles())
         {
-            throw new Exception("No se puede mezclar si aun hay cartas disponibles");
+            throw new IllegalStateException("No se puede mezclar si aun hay cartas disponibles");
         }
         int cantidadDeIntercambios = 100;
-        Random random = new Random();
 
         for(int i = 0; i < cantidadDeIntercambios; i++)
         {
-            int indiceMazo1 = random.nextInt(this.cartasUtilizadas.getTamanio()) + 1;
+            int indiceMazo1 = Utiles.obtenerEnteroAleatorio(1,
+                                                         this.cartasUtilizadas.getTamanio());
             int indiceMazo2 = 0;
             do
             {
-                indiceMazo2 = random.nextInt(this.cartasUtilizadas.getTamanio()) + 1;
+                indiceMazo2 = Utiles.obtenerEnteroAleatorio(1,
+                                                         this.cartasUtilizadas.getTamanio());
             } while(indiceMazo2 == indiceMazo1);
     
             cartasUtilizadas.intercambiar(indiceMazo1, indiceMazo2);
@@ -105,8 +145,20 @@ public class Mazo {
 
 
     }
+    /**
+     * @return devuelve verdadero si hay cartas en el mazo de cartas disponibles
+     */
     public boolean hayCartasDisponibles()
     {
         return this.cartasDisponibles.contarElementos() > 0;
+    }
+
+    /**
+     * 
+     * @return devuelve verdadero si el mazo tiene todas las cartas disponibles
+     */
+    public boolean estaCompleto()
+    {
+        return this.cartasDisponibles.contarElementos() == this.tamanio;
     }
 }
