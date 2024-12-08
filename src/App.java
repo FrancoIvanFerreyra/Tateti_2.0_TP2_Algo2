5890c856d1e0e5d07265b8f3d780934ff17ba5be
Franco Iván Ferreyra <fiferreyra@fi.uba.ar>
2024-12-08 13:49:21 -0300

    Permitir al usuario jugar de nuevo, y reutilizar la configuracion personalizada que creo si asi lo hizo. Agregar mensaje de salida

--- src/App.java
+++ src/App.java
@@ -1,8 +1,10 @@
 import estructuras.Lista;
 import estructuras.ListaSimplementeEnlazada;
 import interfaz.Consola;
+import interfaz.Teclado;
 import tateti.ConfiguracionPartida;
 import tateti.Tateti;
+import utiles.Utiles;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -47,93 +49,100 @@ public class App {
                              3,
                               3,
                                3));
-        boolean usuarioEligioConfiguracion = false;
-        Tateti tateti = null;
-        while(!usuarioEligioConfiguracion)
-        {
-            Consola.imprimirMensaje("Tateti 2.0 - Hecho por Todos Desarrollamos Algo");
-            ConfiguracionPartida opcion = Consola.consultarOpcionAlUsuario(opcionesTateti,
-             "Selecciona una configuracion de partida:", false);
-    
-            if(opcion.getTitulo().equals("Crear nueva configuracion"))
+        do {
+            boolean usuarioEligioConfiguracion = false;
+            Tateti tateti = null;
+
+            while(!usuarioEligioConfiguracion)
             {
-                anchoTablero = Consola.obtenerNumeroEnteroEnRangoMinimoDelUsuario(
-                    "De cuantos casilleros debe ser el ancho del tablero? (Numero mayor o igual a 3) ",
-                3);
-                altoTablero = Consola.obtenerNumeroEnteroEnRangoMinimoDelUsuario(
-                    "De cuantos casilleros debe ser el alto del tablero? (Numero mayor o igual a 3) ",
-                3);
-                profundidadTablero = Consola.obtenerNumeroEnteroEnRangoMinimoDelUsuario(
-                    "De cuantos casilleros debe ser la profundidad del tablero? (Numero mayor o igual a 3) ",
-                3);
-
-                int longitudMaximaTateti = ConfiguracionPartida.obtenerLongitudMaximaDeTateti(
-                    anchoTablero, altoTablero, profundidadTablero);
-
-                cantidadFichasSeguidasParaGanar = Consola.obtenerNumeroEnteroEnRangoDelUsuario(
-                        "Cuantos fichas seguidas para ganar? (Numero entre 3 y " +
-                         longitudMaximaTateti + ") ",
-                         3,
-                          longitudMaximaTateti);
-
-                int cantidadMaximaDeJugadoresParaElTablero = ConfiguracionPartida.
-                          obtenerCantidadMaximaDeJugadoresParaElTablero(
-                anchoTablero, altoTablero, profundidadTablero, cantidadFichasSeguidasParaGanar);
-
-                cantidadJugadores = Consola.obtenerNumeroEnteroEnRangoDelUsuario(
-                    "Cuantos jugadores? (Numero entre 2 y " + 
-                    cantidadMaximaDeJugadoresParaElTablero + ") ",2,
-                    cantidadMaximaDeJugadoresParaElTablero);
-
-                int cantidadMaximaDeFichasPorJugadorEnElTablero = ConfiguracionPartida.
-                    obtenerCantidadMaximaDeFichasPorJugadorEnElTablero(
-                    anchoTablero, altoTablero, profundidadTablero,
-                    cantidadFichasSeguidasParaGanar, cantidadJugadores);
-                
-                cantidadDeFichasPorJugador = Consola.obtenerNumeroEnteroEnRangoDelUsuario(
-                        "Cuantos fichas por jugador? (Numero entre " + 
-                        cantidadFichasSeguidasParaGanar + " y " +
-                        cantidadMaximaDeFichasPorJugadorEnElTablero + ") ",
-                         cantidadFichasSeguidasParaGanar, 
-                        cantidadMaximaDeFichasPorJugadorEnElTablero);
+                Consola.imprimirMensajeConSalto("Tateti 2.0 - Hecho por Todos Desarrollamos Algo");
+                ConfiguracionPartida opcion = Consola.consultarOpcionAlUsuario(opcionesTateti,
+                 "Selecciona una configuracion de partida:", false);
         
-                cantidadCartasPorJugador = Consola.obtenerNumeroEnteroEnRangoMinimoDelUsuario(
-                        "Cuantos cartas por jugador? (Numero mayor o igual a 1) ",1);
-
-                String resumenOpcionesIngresadas = "Dimensiones del tablero: (" +
-                                                anchoTablero + ", " +
-                                                altoTablero + ", " + 
-                                                profundidadTablero + ")\n" +
-                 "Cantidad de jugadores: " + cantidadJugadores + "\n" + 
-                 "Cantidad de fichas por jugador: " + cantidadDeFichasPorJugador + "\n" + 
-                 "Cantidad de cartas por jugador: " + cantidadCartasPorJugador + "\n" + 
-                 "Longitud del tateti: " + cantidadFichasSeguidasParaGanar + "\n";
-
-                Consola.imprimirMensaje(resumenOpcionesIngresadas);
-                if(Consola.obtenerConfirmacionDelUsuario("Confirmar configuracion?", false))
+                if(opcion.getTitulo().equals("Crear nueva configuracion"))
                 {
-                    tateti = new Tateti(anchoTablero, altoTablero, profundidadTablero,
-                    cantidadFichasSeguidasParaGanar,
-                    cantidadJugadores,
-                    cantidadDeFichasPorJugador, cantidadCartasPorJugador
-                     );
-                    usuarioEligioConfiguracion = true;
+                    anchoTablero = Consola.obtenerNumeroEnteroEnRangoMinimoDelUsuario(
+                        "De cuantos casilleros debe ser el ancho del tablero? (Numero mayor o igual a 3) ",
+                    3);
+                    altoTablero = Consola.obtenerNumeroEnteroEnRangoMinimoDelUsuario(
+                        "De cuantos casilleros debe ser el alto del tablero? (Numero mayor o igual a 3) ",
+                    3);
+                    profundidadTablero = Consola.obtenerNumeroEnteroEnRangoMinimoDelUsuario(
+                        "De cuantos casilleros debe ser la profundidad del tablero? (Numero mayor o igual a 1) ",
+                    1);
+    
+                    int longitudMaximaTateti = ConfiguracionPartida.obtenerLongitudMaximaDeTateti(
+                        anchoTablero, altoTablero, profundidadTablero);
+    
+                    cantidadFichasSeguidasParaGanar = Consola.obtenerNumeroEnteroEnRangoDelUsuario(
+                            "Cuantos fichas seguidas para ganar? (Numero entre 3 y " +
+                             longitudMaximaTateti + ") ",
+                             3,
+                              longitudMaximaTateti);
+    
+                    int cantidadMaximaDeJugadoresParaElTablero = ConfiguracionPartida.
+                              obtenerCantidadMaximaDeJugadoresParaElTablero(
+                    anchoTablero, altoTablero, profundidadTablero, cantidadFichasSeguidasParaGanar);
+    
+                    cantidadJugadores = Consola.obtenerNumeroEnteroEnRangoDelUsuario(
+                        "Cuantos jugadores? (Numero entre 2 y " + 
+                        cantidadMaximaDeJugadoresParaElTablero + ") ",2,
+                        cantidadMaximaDeJugadoresParaElTablero);
+    
+                    int cantidadMaximaDeFichasPorJugadorEnElTablero = ConfiguracionPartida.
+                        obtenerCantidadMaximaDeFichasPorJugadorEnElTablero(
+                        anchoTablero, altoTablero, profundidadTablero,
+                        cantidadFichasSeguidasParaGanar, cantidadJugadores);
+                    
+                    cantidadDeFichasPorJugador = Consola.obtenerNumeroEnteroEnRangoDelUsuario(
+                            "Cuantos fichas por jugador? (Numero entre " + 
+                            cantidadFichasSeguidasParaGanar + " y " +
+                            cantidadMaximaDeFichasPorJugadorEnElTablero + ") ",
+                             cantidadFichasSeguidasParaGanar, 
+                            cantidadMaximaDeFichasPorJugadorEnElTablero);
+            
+                    cantidadCartasPorJugador = Consola.obtenerNumeroEnteroEnRangoMinimoDelUsuario(
+                            "Cuantos cartas por jugador? (Numero mayor o igual a 1) ",1);
+    
+                    ConfiguracionPartida nuevaConfiguracion = new 
+                                    ConfiguracionPartida(
+                                "Nueva configuracion",
+                                        anchoTablero, altoTablero,
+                                        profundidadTablero,
+                                        cantidadFichasSeguidasParaGanar,
+                                        cantidadJugadores,
+                                        cantidadDeFichasPorJugador,
+                                        cantidadCartasPorJugador);
+    
+                    Consola.imprimirMensajeConSalto(nuevaConfiguracion.getDescripcion());
+                    if(Consola.obtenerConfirmacionDelUsuario("Confirmar configuracion?", false))
+                    {
+                        tateti = new Tateti(nuevaConfiguracion);
+                        opcionesTateti.agregar(nuevaConfiguracion);
+                        usuarioEligioConfiguracion = true;
+                    }
+                }
+                else
+                {
+                    Consola.imprimirMensajeConSalto(opcion.getDescripcion());
+                    if(Consola.obtenerConfirmacionDelUsuario("Confirmar configuracion?", false))
+                    {
+                        tateti = new Tateti(opcion);
+                        usuarioEligioConfiguracion = true;
+                    }
                 }
             }
-            else
+            if(tateti == null)
             {
-                Consola.imprimirMensaje(opcion.getDescripcion());
-                if(Consola.obtenerConfirmacionDelUsuario("Confirmar configuracion?", false))
-                {
-                    tateti = new Tateti(opcion);
-                    usuarioEligioConfiguracion = true;
-                }
+                throw new Exception("No se pudo iniciar la partida correctamente");
             }
+            tateti.jugar();
         }
-        if(tateti == null)
-        {
-            throw new Exception("No se pudo iniciar la partida correctamente");
-        }
-        tateti.jugar();
+        while(Consola.obtenerConfirmacionDelUsuario("Desea jugar otra partida?", false));
+        Consola.imprimirMensaje("Gracias por jugar Tateti 2.0! Saliendo...");
+        Teclado.finalizar();
+        Utiles.pausarEjecucion(1500);
+
+
     }
 }
